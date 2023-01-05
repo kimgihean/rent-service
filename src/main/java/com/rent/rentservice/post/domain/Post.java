73762b4ac0f687f71dd2e54c11cb9c35adf966af
kimgihean <rlgus2738@naver.com>
2023-01-05 21:42:10 +0900

    Initiate Environment
    Entity

--- src/main/java/com/rent/rentservice/post/domain/Post.java
+++ src/main/java/com/rent/rentservice/post/domain/Post.java
@@ -1,10 +1,13 @@
 package com.rent.rentservice.post.domain;
 
 import com.rent.rentservice.user.domain.User;
+import lombok.Data;
+
 import javax.persistence.*;
 import java.sql.Date;
 
 @Entity
+@Data
 public class Post {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long postID;
@@ -15,45 +18,6 @@ public class Post {
     private Date period;
     private int favorite;
 
-    public Long getPostID() {
-        return postID;
-    }
-
-    public void setPostID(Long postID) {
-        this.postID = postID;
-    }
-
-    public User getUserID() {
-        return userID;
-    }
-
-    public void setUserID(User userID) {
-        this.userID = userID;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Date getPeriod() {
-        return period;
-    }
-
-    public void setPeriod(Date period) {
-        this.period = period;
-    }
-
-    public int getFavorite() {
-        return favorite;
-    }
-
-    public void setFavorite(int favorite) {
-        this.favorite = favorite;
-    }
     /*
     @OneToMany
     @JoinColumn(name = "ADDRESSID")
--- src/main/java/com/rent/rentservice/user/domain/User.java
+++ src/main/java/com/rent/rentservice/user/domain/User.java
@@ -1,8 +1,11 @@
 package com.rent.rentservice.user.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Entity
+@Data
 @Table(name = "USER")
 public class User {
 
@@ -13,44 +16,4 @@ public class User {
     private String phoneNumber;
     private String email;
     //private String image;
-
-    public void setId(Long userId) {
-        this.userId = userId;
-    }
-
-    public Long getId() {
-        return userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
