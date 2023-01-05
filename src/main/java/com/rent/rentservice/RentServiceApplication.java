c3bc7bdcf4003eb0847de88113e06af1a35476ae
kimgihean <rlgus2738@naver.com>
2023-01-05 18:35:55 +0900

    Initiate Environment

---
+++ src/main/java/com/rent/rentservice/RentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rent.rentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentServiceApplication.class, args);
+    }
+
+}
