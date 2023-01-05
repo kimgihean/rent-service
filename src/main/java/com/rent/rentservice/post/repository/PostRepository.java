a60786543cf8ccdd1926193435b4c395036c0216
kimgihean <rlgus2738@naver.com>
2023-01-05 21:46:47 +0900

    Initiate Environment
    Entity

--- src/main/java/com/rent/rentservice/post/repository/PostRepository.java
+++ src/main/java/com/rent/rentservice/post/repository/PostRepository.java
@@ -1,11 +1,9 @@
 package com.rent.rentservice.post.repository;
 
 import com.rent.rentservice.post.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface PostRepository {
-    Post save(Post post);
-    Optional<Post> findById(Long id);
-    //Optional<Post> findBy();
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
--- src/main/java/com/rent/rentservice/user/repository/UserRepository.java
+++ src/main/java/com/rent/rentservice/user/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.rent.rentservice.user.repository;
 
-public interface UserRepository {
+import com.rent.rentservice.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
