3c270b2cf0499e57b8e9662f5d85bdd425593b3e
KimSeungJin <dahyun101107@naver.com>
2023-01-14 20:10:17 +0900

    2023.01.14 김승진
    1. 로그인 테스트 코드 작성

--- src/test/java/com/rent/rentservice/user/controller/UserControllerTest.java
+++ src/test/java/com/rent/rentservice/user/controller/UserControllerTest.java
@@ -4,13 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rent.rentservice.user.domain.User;
 import com.rent.rentservice.user.repository.UserRepository;
 import com.rent.rentservice.user.request.JoinForm;
+import com.rent.rentservice.user.request.LoginForm;
 import com.rent.rentservice.util.encryption.AES256;
+import com.rent.rentservice.util.session.SessionUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
@@ -85,7 +88,6 @@ class UserControllerTest {
         assertEquals("test@test.com", user.getEmail());
         assertEquals("1234", aes256.decrypt(user.getPassword()));
         assertEquals("경기도", user.getAddress());
-
     }
 
     @Test
@@ -114,6 +116,69 @@ class UserControllerTest {
                 .andExpect(jsonPath("$.message").value("입력되지 않은 값이 존재합니다"))
                 .andExpect(jsonPath("$.vaildation.nickName").value("닉네임을 입력해주세요"))
                 .andExpect(jsonPath("$.vaildation.phoneNumber").value("전화번호를 입력해주세요"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("로그인 요청")
+    void test3() throws Exception {
+
+        MockHttpSession session = new MockHttpSession();
+
+        //given
+        User user = User.builder()
+                .name("홍길동")
+                .nickName("닉네임")
+                .email("test@test.com")
+                .phoneNumber("01012345678")
+                .password(aes256.encrypt("1234"))
+                .address("경기도")
+                .build();
+
+        userRepository.save(user);
+
+        LoginForm request = LoginForm.builder()
+                .email("test@test.com")
+                .password("1234")
+                .build();
+
+        String json = objectMapper.writeValueAsString(request);
+
+        //when
+        mockMvc.perform(post("/api/v1/login")
+                        .session(session)
+                        .contentType(APPLICATION_JSON)
+                        .content(json)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        //then
+        assertEquals("닉네임", SessionUtil.getLoginMemberNickname(session));
+        assertEquals("test@test.com", SessionUtil.getLoginMemberEmail(session));
+    }
+
+    @Test
+    @DisplayName("로그인 파라미터 Vaild 테스트")
+    void test4() throws Exception {
+
+        //given
+        LoginForm request = LoginForm.builder()
+                .email("")
+                .password("1234")
+                .build();
+
+        String json = objectMapper.writeValueAsString(request);
+
+        //expected
+        mockMvc.perform(post("/api/v1/login")
+                        .contentType(APPLICATION_JSON)
+                        .content(json)
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.message").value("입력되지 않은 값이 존재합니다"))
+                .andExpect(jsonPath("$.vaildation.email").value("이메일을 입력해주세요"))
                 .andDo(print());
     }
 }
--- src/test/java/com/rent/rentservice/user/service/UserServiceTest.java
+++ src/test/java/com/rent/rentservice/user/service/UserServiceTest.java
@@ -3,14 +3,18 @@ package com.rent.rentservice.user.service;
 import com.rent.rentservice.user.domain.User;
 import com.rent.rentservice.user.exception.InvalidEmailPatternException;
 import com.rent.rentservice.user.exception.OverlapUserEmailException;
+import com.rent.rentservice.user.exception.UserNotFoundException;
 import com.rent.rentservice.user.repository.UserRepository;
 import com.rent.rentservice.user.request.JoinForm;
+import com.rent.rentservice.user.request.LoginForm;
 import com.rent.rentservice.util.encryption.AES256;
+import com.rent.rentservice.util.session.SessionUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -107,6 +111,73 @@ class UserServiceTest {
         //expected
         assertThrows(InvalidEmailPatternException.class, () -> {
             userService.join(request);
+        });
+    }
+
+    @Test
+    @DisplayName("로그인")
+    void test4() throws Exception {
+
+        //given
+        MockHttpSession session = new MockHttpSession();
+
+        User user = User.builder()
+                .name("홍길동")
+                .nickName("닉네임")
+                .email("test@test.com")
+                .phoneNumber("01012345678")
+                .password(aes256.encrypt("1234"))
+                .address("경기도")
+                .build();
+
+        userRepository.save(user);
+
+        LoginForm request = LoginForm.builder()
+                .email("test@test.com")
+                .password("1234")
+                .build();
+
+        //when
+        userService.login(request, session);
+
+        //then
+        assertEquals("test@test.com", SessionUtil.getLoginMemberEmail(session));
+        assertEquals("닉네임", SessionUtil.getLoginMemberNickname(session));
+    }
+
+    @Test
+    @DisplayName("로그인 - 이메일 형식 아님")
+    void test5() throws Exception {
+
+        //given
+        MockHttpSession session = new MockHttpSession();
+
+        LoginForm request = LoginForm.builder()
+                .email("test.com")
+                .password("1234")
+                .build();
+
+        //expected
+        assertThrows(InvalidEmailPatternException.class, () -> {
+            userService.login(request, session);
+        });
+    }
+
+    @Test
+    @DisplayName("로그인 - 유저 정보 조회 실패")
+    void test6() throws Exception {
+
+        //given
+        MockHttpSession session = new MockHttpSession();
+
+        LoginForm request = LoginForm.builder()
+                .email("test@test.com")
+                .password("1234")
+                .build();
+
+        //expected
+        assertThrows(UserNotFoundException.class, () -> {
+            userService.login(request, session);
         });
     }
 }
